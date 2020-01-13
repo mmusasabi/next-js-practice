1a8999546d7a51d845135bdd194ab28033a777d1
mmusasabi <mmusasabi@gmail.com>
2020-01-13 18:26:50 +0900

    update reduxのサンプルをそのまま突っ込んでみた

--- app/next/components/nav.js
+++ app/next/components/nav.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 
 const links = [
   { href: 'https://zeit.co/now', label: 'ZEIT' },
+  { href: '/redux', label: 'Redux' },
   { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
